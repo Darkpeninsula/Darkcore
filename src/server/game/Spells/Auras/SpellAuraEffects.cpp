80b3614a8eb1e4dc666b8f4d85498e9948a12ca4
Nevan <site@darkpeninsula.eu>
2012-03-16 08:34:50 +0100

    Another Fix Compile

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -2861,8 +2861,8 @@ void AuraEffect::HandleAuraMounted(AuraApplication const* aurApp, uint8 mode, bo
 
         if (aurApp->GetBase()->GetId() == 87840)
         {
-            target->Mount(plr->getGender() == GENDER_FEMALE ? 29423 : 29422, 0, GetMiscValue());
-            target->Mount(plr->getGender() == GENDER_MALE ? 29422 : 29423, 0, GetMiscValue());
+            target->Mount(player->getGender() == GENDER_FEMALE ? 29423 : 29422, 0, GetMiscValue());
+            target->Mount(player->getGender() == GENDER_MALE ? 29422 : 29423, 0, GetMiscValue());
             return;
         }
 
@@ -6106,7 +6106,7 @@ void AuraEffect::HandleAuraSwapSpells(AuraApplication const * aurApp, uint8 mode
 
     ActionBarSpellOverride const* spellOverride = sSpellMgr->GetActionBarSpellOverride(overrideId);
 
-    if(!actionbar)
+    if(!spellOverride)
         return;
     
     uint32 aura = spellOverride->aura;
