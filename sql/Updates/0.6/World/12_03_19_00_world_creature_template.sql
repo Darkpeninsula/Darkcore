b6ff96580a5ea70b5d2606d4846dbf347357bf23
R3v3ng4 <garu_87@live.it>
2012-03-19 20:20:07 +0100

    Sql Grim Batol e Trhone of The tides

---
+++ sql/Updates/0.6/World/12_03_19_00_world_creature_template.sql
@@ -0,0 +1,19 @@
+UPDATE `creature_template`
+SET `ScriptName` = 'mob_valiona_gb'
+WHERE `entry` =40320;
+
+UPDATE `creature_template`
+SET `ScriptName` = 'mob_invoked_flame_spirit'
+WHERE `entry` =40357;
+
+UPDATE `creature_template`
+SET `ScriptName` = 'mob_faceless'
+WHERE `entry` =48844;
+
+UPDATE `creature_template`
+SET `ScriptName` = 'mob_alexstraszas_eggs'
+WHERE `entry` =40486;
+
+UPDATE `creature_template`
+SET `ScriptName` = 'mob_shadow_gale_stalker'
+WHERE `entry` =39388;
