2faa1a713fab50a2db82119f45c7732efd3c4f7c
Nevan <site@darkpeninsula.eu>
2012-04-02 23:06:47 +0200

    Cleanup Copyright

--- src/server/shared/Packets/WorldPacket.cpp
+++ src/server/shared/Packets/WorldPacket.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2011-2012 DarkCore <http://www.darkpeninsula.eu/>
  * Copyright (C) 2011-2012 Project SkyFire <http://www.projectskyfire.org/>
  * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/> *
+ * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
