3ca2bd021e0cc80291eabad8d006aa951b52a676
Nevan <site@darkpeninsula.eu>
2012-04-10 01:05:56 +0200

    Cleanup Last Commit

--- src/server/scripts/EasternKingdoms/Gilneas/gilneas.cpp
+++ src/server/scripts/EasternKingdoms/Gilneas/gilneas.cpp
@@ -549,23 +549,19 @@ class go_merchant_square_door : public GameObjectScript
 public:
     go_merchant_square_door() : GameObjectScript("go_merchant_square_door") {}
 
-    float x, y, z, wx, wy, angle, tQuestCredit;
-    bool opened;
+    float x, y, z, wx, wy, angle;
     uint8 spawnKind;
-    Player* aPlayer;
-    GameObject* go;
-    uint32 DoorTimer;
 
     bool OnGossipHello(Player* player, GameObject* go)
     {
+        if(!player)
+            return;
+
         if (player->GetQuestStatus(QUEST_EVAC_MERC_SQUA) == QUEST_STATUS_INCOMPLETE && go->GetGoState() == GO_STATE_READY)
         {
-            aPlayer          = player;
-            opened           = 1;
-            tQuestCredit     = 2500;
+            spawnKind = urand(0, 1);
+
             go->SetGoState(GO_STATE_ACTIVE);
-            DoorTimer = DOOR_TIMER;
-            spawnKind = urand(1, 3); //1, 2=citizen, 3=citizen&worgen (66%, 33%)
             angle = go->GetOrientation();
             x = go->GetPositionX()-cos(angle)*2;
             y = go->GetPositionY()-sin(angle)*2;
@@ -573,61 +569,35 @@ public:
             wx = x-cos(angle)*2;
             wy = y-sin(angle)*2;
 
-            if(player)
-                player->KilledMonsterCredit(35830, 0);
-
-            if (spawnKind < 3)
+            if (spawnKind)
             {
-                if (Creature* spawnedCreature = go->SummonCreature(NPC_FRIGHTENED_CITIZEN_1, x, y, z, angle, TEMPSUMMON_TIMED_DESPAWN, SUMMON1_TTL))
+                if (Creature* spawnedCitizen = go->SummonCreature(NPC_FRIGHTENED_CITIZEN_1, x, y, z, angle, TEMPSUMMON_TIMED_DESPAWN, SUMMON1_TTL))
                 {
-                    spawnedCreature->SetPhaseMask(6, 1);
-                    spawnedCreature->Respawn(1);
+                    spawnedCitizen->SetPhaseMask(6, 1);
+                    spawnedCitizen->Respawn(1);
+
+                    if (Creature* spawnedWorgen = go->SummonCreature(NPC_RAMPAGING_WORGEN_2, wx, wy, z, angle, TEMPSUMMON_TIMED_DESPAWN, SUMMON1_TTL))
+                    {
+                        spawnedWorgen->SetPhaseMask(6, 1);
+                        spawnedWorgen->Respawn(1);
+                        spawnedWorgen->getThreatManager().resetAllAggro();
+                        spawnedWorgen->AddThreat(spawnedCitizen, 1.0f);
+                    }
                 }
             }
             else
             {
-                if (Creature* spawnedCreature = go->SummonCreature(NPC_FRIGHTENED_CITIZEN_2, x, y, z, angle, TEMPSUMMON_TIMED_DESPAWN, SUMMON1_TTL))
+                if (Creature* spawnedCitizen = go->SummonCreature(NPC_FRIGHTENED_CITIZEN_2, x, y, z, angle, TEMPSUMMON_TIMED_DESPAWN, SUMMON1_TTL))
                 {
-                    spawnedCreature->SetPhaseMask(6, 1);
-                    spawnedCreature->Respawn(1);
+                    spawnedCitizen->SetPhaseMask(6, 1);
+                    spawnedCitizen->Respawn(1);
                 }
             }
+
+            player->KilledMonsterCredit(35830, 0);
             return true;
         }
         return false;
-    }
-
-    void OnUpdate(GameObject* go, uint32 diff)
-    {
-        if (opened == 1)
-        {
-            if (tQuestCredit <= ((float)diff/8))
-            {
-                opened = 0;
-                //aPlayer->KilledMonsterCredit(35830, 0);
-                if (spawnKind == 3)
-                {
-                    if (Creature* spawnedCreature = go->SummonCreature(NPC_RAMPAGING_WORGEN_2, wx, wy, z, angle, TEMPSUMMON_TIMED_DESPAWN, SUMMON1_TTL))
-                    {
-                        spawnedCreature->SetPhaseMask(6, 1);
-                        spawnedCreature->Respawn(1);
-                        spawnedCreature->getThreatManager().resetAllAggro();
-                        aPlayer->AddThreat(spawnedCreature, 1.0f);
-                        spawnedCreature->AddThreat(aPlayer, 1.0f);
-                    }
-                }
-            }
-            else tQuestCredit -= ((float)diff/8);
-        }
-        if (DoorTimer <= diff)
-            {
-                if (go->GetGoState() == GO_STATE_ACTIVE)
-                    go->SetGoState(GO_STATE_READY);
-
-                DoorTimer = DOOR_TIMER;
-            }
-        else
-            DoorTimer -= diff;
     }
 };
 
