2d97b0c0c71ffb608aed1f822e58709005ac756f
Nevan <site@darkpeninsula.eu>
2012-04-18 11:49:46 +0200

    Implement Blood Shield Cleanup Death Strike Formula

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -858,7 +858,7 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
                                 spellId = 83239;
                                 break;
                         }
-                        
+
                         if(spellId && !m_caster->HasAura(spellId))
                             m_caster->CastSpell(m_caster, spellId, true);
                     }
@@ -1788,10 +1788,7 @@ void Spell::EffectDummy(SpellEffIndex effIndex)
             // Death strike
             if (m_spellInfo->SpellFamilyFlags[0] & SPELLFAMILYFLAG_DK_DEATH_STRIKE)
             {
-                if(((m_caster->GetDamageTakenInPastSecs(5) * 15) / 100) > m_caster->CountPctFromMaxHealth(damage))
-                    bp = ((m_caster->GetDamageTakenInPastSecs(5) * 15) / 100);
-                else
-                    bp = m_caster->CountPctFromMaxHealth(damage);
+                bp = std::min<int32>(m_caster->CountPctFromMaxHealth(damage), CalculatePctN(m_caster->GetDamageTakenInPastSecs(5), 15));
 
                 // Improved Death Strike
                 if (AuraEffect const* aurEff = m_caster->GetAuraEffect(SPELL_AURA_ADD_PCT_MODIFIER, SPELLFAMILY_DEATHKNIGHT, 2751, 0))
@@ -1801,7 +1798,18 @@ void Spell::EffectDummy(SpellEffIndex effIndex)
                 if (AuraEffect const* aurEff = m_caster->GetAuraEffect(96279, 0))
                     if (bp < int32(m_caster->CountPctFromMaxHealth(aurEff->GetAmount())))
                         if (m_caster->HasAura(48265) || m_caster->HasAura(48266)) // Only in frost/unholy presence
-                            bp = m_caster->CountPctFromMaxHealth(aurEff->GetAmount());
+                            bp += m_caster->CountPctFromMaxHealth(aurEff->GetAmount());
+
+                // Blood Shield
+                if (AuraEffect const* aurEff = m_caster->GetAuraEffect(77513, 1))
+                {
+                    // Blood Presence
+                    if (m_caster->HasAura(48263))
+                    {
+                        int32 shield = CalculatePctN(bp, int32(aurEff->GetAmount() * m_caster->ToPlayer()->GetMasteryPoints()));
+                        m_caster->CastCustomSpell(m_caster, 77535, &shield, NULL, NULL, false);
+                    }
+                }
 
                 m_caster->CastCustomSpell(m_caster, 45470, &bp, NULL, NULL, false);
                 return;
