356d124a71d84448cb8cebb0fb9a156325a78e74
Defrag <Thema89@email.it>
2012-05-03 20:49:28 +0200

    Correct last commit, sorry

--- sql/updates/0.7/World/12_05_03_00_item_loot_template.sql
+++ sql/updates/0.7/World/12_05_03_00_item_loot_template.sql
@@ -1,8 +1,8 @@
 -- Fix Abandoned Adventurer's Satchel item loot
 
-DELETE FROM item_template_addon WHERE entry = 44663;
+DELETE FROM item_template_addon WHERE id = 44663;
 DELETE FROM item_loot_template WHERE entry = 44663;
-INSERT INTO item_template_addon VALUES (44663, 1, 0, 150000, 250000);
+INSERT INTO item_template_addon VALUES (44663, 1, 0, 150000, 250000, 0);
 INSERT INTO item_loot_template VALUES
 (44663, 33470, 100, 1, 0, 20, 20),
 (44663, 37700, 10, 1, 1, 3, 5),
