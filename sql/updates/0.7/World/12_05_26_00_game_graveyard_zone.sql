9a8d68c6a218b31bc7b33dbc10035160f56224a3
Darkrabbit <scazzato88@gmail.com>
2012-05-26 01:58:55 +0200

    [Core/DB] Add More Graveryard:
    * Uldum-The Vortex Pinnacle
    * Deepholm-The Stonecore
    * Uldum-Lost City of the Tol'vir
    * Uldum-Halls of Origination
    * Uldum-Throne of the Four Winds
    * Twilight Highlands-The Bastion of Twilight
    * Abyssal Depths-Throne of the Tides
    * Blackrock Mountain-Blackrock Caverns
    * Blackrock Mountain-Blackwing Descent
    * Mount Hyjal-Firelands

---
+++ sql/updates/0.7/World/12_05_26_00_game_graveyard_zone.sql
@@ -0,0 +1,62 @@
+-- Uldum-The Vortex Pinnacle
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1856', '5035', '0');
+
+-- Deepholm-The Stonecore
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1756', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1757', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1758', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1790', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1791', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1792', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1793', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1794', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1795', '5088', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1796', '5088', '0');
+
+-- Uldum-Lost City of the Tol'vir
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1818', '5396', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1819', '5396', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1820', '5396', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1821', '5396', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1822', '5396', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1823', '5396', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1824', '5396', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1856', '5396', '0');
+
+-- Uldum-Halls of Origination
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1818', '4945', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1819', '4945', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1820', '4945', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1821', '4945', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1822', '4945', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1823', '4945', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1824', '4945', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1856', '4945', '0');
+
+-- Uldum-Throne of the Four Winds
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1818', '5638', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1819', '5638', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1820', '5638', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1821', '5638', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1822', '5638', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1823', '5638', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1824', '5638', '0');
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1856', '5638', '0');
+
+-- Twilight Highlands-The Bastion of Twilight
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1846', '5334', '0');
+
+-- Twilight Highlands-The Bastion of Twilight
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1812', '4950', '0');
+
+-- Abyssal Depths-Throne of the Tides
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1778', '5004', '0');
+
+-- Blackrock Mountain-Blackrock Caverns
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('636', '4926', '0');
+
+-- Blackrock Mountain-Blackwing Descent
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('636', '5094', '0');
+
+-- Mount Hyjal-Firelands
+INSERT IGNORE INTO `game_graveyard_zone` VALUES ('1755', '5723', '0');
