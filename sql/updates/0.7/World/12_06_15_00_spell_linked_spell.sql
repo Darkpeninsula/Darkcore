293c040bb582794aa52ab9862f5ab49f03df8c27
Defrag <Thema89@email.it>
2012-06-15 00:43:49 +0200

    DB/Spells: added DB support for prev commit, FULL working

---
+++ sql/updates/0.7/World/12_06_15_00_spell_linked_spell.sql
@@ -0,0 +1,6 @@
+
+-- Lay on Hands, Divine Shield and Hand of Protection triggers Forbearance
+INSERT INTO spell_linked_spell VALUES
+(633, 25771, 0, "Lay on Hands -> Forbearance");
+(642, 25771, 0, "Divine Shield -> Forbearance"),
+(1022, 25771, 0, "Hand of Protection -> Forbearance");
