777f20c96203e4517f04b1a3c757f616e41cd083
Drethek <sss.nevan@gmail.com>
2012-06-15 07:56:29 +0200

    Fix Nether Vortex Proc

---
+++ sql/updates/0.7/World/12_06_15_01_spell_linked_spell.sql
@@ -0,0 +1,7 @@
+-- (86181) Nether Vortex (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (86181);
+INSERT INTO `spell_proc_event` VALUES (86181, 0x00, 0x03, 0x20000000, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 0, 0);
+
+-- (86209) Nether Vortex (Rank 2)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (86209);
+INSERT INTO `spell_proc_event` VALUES (86209, 0x00, 0x03, 0x20000000, 0x00000000, 0x00000000, 0x00010000, 0x00000000, 0, 0, 0);
