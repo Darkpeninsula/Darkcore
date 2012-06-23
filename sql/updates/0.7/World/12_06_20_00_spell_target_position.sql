9eb3815c5d2123f6150decc382fc85a91ff6a57b
Thema89 <Thema89@email.it>
2012-06-23 18:51:19 +0200

    DB/Spell: Update teleport position for tabard 63379

--- sql/updates/0.7/World/12_06_20_00_spell_target_position.sql
+++ sql/updates/0.7/World/12_06_20_00_spell_target_position.sql
@@ -6,4 +6,5 @@ UPDATE gameobject_template SET data0=88339 WHERE entry=206616; -- horde portal
 DELETE FROM spell_target_position WHERE id IN (88342, 88344);
 INSERT INTO spell_target_position VALUES
 (88342, 732, -334.60, 1043.80, 21.900, 1.5), -- ally teleport
-(88344, 732, -601.40, 1382.03, 21.900, 1.5); -- horde teleport
+(88344, 732, -601.40, 1382.03, 21.900, 1.5), -- horde teleport
+(89597, 732, -334.60, 1043.80, 21.900, 1.5); -- Baradin's Wardens Tabard (63379)
