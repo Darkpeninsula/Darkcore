c2094ea2fe9d1c21d8d2d39e7187a466f08083b7
Thema89 <Thema89@email.it>
2012-06-29 20:20:58 +0200

    Core/StatSyS: Initial cleanup for mastery calculation

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -730,20 +730,29 @@ void Player::UpdateArmorPenetration(int32 amount)
     SetUInt32Value(PLAYER_FIELD_COMBAT_RATING_1 + CR_ARMOR_PENETRATION, amount);
 }
 
-void Player::UpdateMasteryPercentage()
+/* void Player::UpdateMasteryPercentage()
 {
     // No mastery
-    float value = 0.0f;
+    float value = 0.0f, basevalue = 0.0f;
     if (CanMastery())
     {
         value = 0.0f;
         // Mastery from SPELL_AURA_MASTERY aura
-        value += GetTotalAuraModifier(SPELL_AURA_MASTERY);
+        basevalue += GetTotalAuraModifier(SPELL_AURA_MASTERY);
         // Mastery from rating
         value += GetRatingBonusValue(CR_MASTERY);
-        value = value < 0.0f ? 0.0f : value;
+        value = ((value - basevalue) < 0.0f ? basevalue : value);
     }
     SetFloatValue(PLAYER_MASTERY, value);
+} */
+
+void Player::UpdateMasteryPercentage()
+{
+    if (CanMastery())
+    {
+        SetInt32Value(PLAYER_FIELD_COMBAT_RATING_1 + CR_MASTERY, _baseRatingValue[CR_MASTERY]);
+        SetFloatValue(PLAYER_MASTERY, GetMasteryPoints());
+    }
 }
 
 void Player::UpdateMeleeHitChances()
@@ -878,14 +887,6 @@ void Player::_RemoveAllStatBonuses()
     UpdateAllStats();
 }
 
-/*void Player::UpdateMastery()
-{
-    if (HasAuraType(SPELL_AURA_MASTERY))
-    {
-        SetInt32Value(PLAYER_FIELD_COMBAT_RATING_1 + CR_MASTERY, _baseRatingValue[CR_MASTERY]);
-        SetFloatValue(PLAYER_MASTERY, GetMasteryPoints());
-    }
-}*/
 
 /*#######################################
 ########                         ########
