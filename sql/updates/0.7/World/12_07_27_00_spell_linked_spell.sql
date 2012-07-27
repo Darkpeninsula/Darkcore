353cd27d00f58ca2470fe56dbe05790b2eed228c
Walkum <juanmavictoria@gmail.com>
2012-07-27 15:17:25 +0200

    DB/Spells: Fixed Soulburn aura remove when you use the improved spell
    Thanks to Darkrabbit for the help :D

---
+++ sql/updates/0.7/World/12_07_27_00_spell_linked_spell.sql
@@ -0,0 +1,12 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_effect` = -74434;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`)
+VALUES
+	(6353, -74434, 0, 'Warlock - Soulburn (Soul Fire)'),
+	(689, -74434, 0, 'Warlock - Soulburn (Drain Life)'),
+	(688, -74434, 0, 'Warlock - Soulburn (Summon Imp)'),
+	(697, -74434, 0, 'Warlock - Soulburn (Summon Voidwalker)'),
+	(712, -74434, 0, 'Warlock - Soulburn (Summon Succubus)'),
+	(691, -74434, 0, 'Warlock - Soulburn (Summon Felhunter)'),
+	(30146, -74434, 0, 'Warlock - Soulburn (Summon Felguard)'),
+	(48020, -74434, 0, 'Warlock - Soulburn (Demonic Circle: Teleport)'),
+	(5676, -74434, 0, 'Warlock - Soulburn (Searing Pain)');
