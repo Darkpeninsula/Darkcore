e76f116778f6262587713c2b6a44bccb733378b7
Nevan <sss.nevan@gmail.com>
2012-08-30 03:27:33 +0300

    Fix Master Marksman (Aimed Shoot)

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3410,6 +3410,7 @@ void SpellMgr::LoadSpellCustomAttr()
             case 19975: // Entangling Roots (Rank 1) -- Nature's Grasp Proc
             case 27010: // Entangling Roots (Rank 7) -- Nature's Grasp Proc
             case 53313: // Entangling Roots (Rank 8) -- Nature's Grasp Proc
+            case 82928: // Aimed Shot! - Master Marksman
                 spellInfo->CastTimeEntry = sSpellCastTimesStore.LookupEntry(1);
                 break;
             case 61719: // Easter Lay Noblegarden Egg Aura - Interrupt flags copied from aura which this aura is linked with
